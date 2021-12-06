8c8a8efd3bbdce7091f203935b1087d5962cd21f
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-12-06 08:57:40 +0530

    Fetching and formatting weatherData

--- app.js
+++ app.js
@@ -13,15 +13,24 @@ app.get("/", function (req, res) {
 
         response.on("data", function (data) {
             const weatherData = JSON.parse(data);
+            const cityName = weatherData.name;
             const temp = weatherData.main.temp;
             const tempDesc = weatherData.weather[0].description;
-            
+            const icon = weatherData.weather[0].icon;
+
+            const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+            res.write("<p>The weather is currently " + tempDesc + "</p>");
+            res.write("<h1>The temperature in " + cityName + " is " + temp + " degree celcius.</h1>");
+            res.write("<img src="+ imgURL +">");
+
+            res.send();
             console.log(temp);
             console.log(tempDesc);
         });
     });
    
-    res.send("Hello");
+    
 });
 
 
