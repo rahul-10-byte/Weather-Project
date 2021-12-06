ed45741bae110d601361c5eac2ce12e9ee6993f9
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-12-06 08:27:09 +0530

    parsing JSON

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion: 6
 
 const express = require("express");
+const { write } = require("fs");
 const https = require("https");
  
 const app = express();
@@ -9,7 +10,15 @@ app.get("/", function (req, res) {
 
     const url = "https://api.openweathermap.org/data/2.5/weather?q=aurangabad&units=metric&appid=321f82581bd9830ec1119d4992566d66";
     https.get(url, function (response) {
-        console.log(response);
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const tempDesc = weatherData.weather[0].description;
+            
+            console.log(temp);
+            console.log(tempDesc);
+        });
     });
    
     res.send("Hello");
