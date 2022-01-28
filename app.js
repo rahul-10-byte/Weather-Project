d22a8ae9d055d115c7ef80e4e8272d0478c669c9
rahul-10-byte <kalyankarrahul500@gmail.com>
2022-01-28 12:36:39 +0530

    Deployed to heroku

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.post("/", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("server started Successfully at 3000!!!");
 });
